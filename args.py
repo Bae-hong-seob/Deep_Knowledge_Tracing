8231a89b1049d140cc8c213c2a8326e1d470112a
bae-hong-seob <bhs8931@gmail.com>
2024-01-07 10:08:23 +0000

    add: autoglon code

--- args.py
+++ args.py
@@ -9,7 +9,7 @@ def parse_args():
     arg('--data_dir', type=str, default='data/', help='Data path를 설정할 수 있습니다.')
     arg('--saved_model_path', type=str, default='./saved_models', help='Saved Model path를 설정할 수 있습니다.')
     
-    arg('--model', type=str, choices=['XGB', 'LIGHTGBM','CATBOOST'],help='학습 및 예측할 모델을 선택할 수 있습니다.')
+    arg('--model', type=str, default='LIGHTGBM', choices=['XGB', 'LIGHTGBM','CATBOOST'], help='학습 및 예측할 모델을 선택할 수 있습니다.')
     arg('--data_shuffle', type=bool, default=True, help='데이터 셔플 여부를 조정할 수 있습니다.')
     arg('--test_size', type=float, default=0.2, help='Train/Valid split 비율을 조정할 수 있습니다.')
     arg('--seed', type=int, default=42, help='seed 값을 조정할 수 있습니다.')
@@ -20,7 +20,7 @@ def parse_args():
     arg("--lr", default=0.001, type=float, help="")
     arg("--model_dir", default="./models/", type=str, help="")
     arg("--model_name", default="best_model.pt", type=str, help="")
-    arg('--optuna', type=bool, default=False, help='하이퍼 파라미터 자동 최적화 설정입니다.')
+    arg('--autogluon', type=bool, default=False, help='하이퍼 파라미터 자동 최적화 설정입니다.')
     
     ############### LightGBM OPTION
     arg("--n_estimators", default=1000, type=int, help="n_estimators")
--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from src.data_preprocess.lightgbm_data import lightgbm_dataloader, lightgbm_prep
 from src.data_preprocess.catboost_data import catboost_dataloader, catboost_preprocess_data,catboost_datasplit
 from src.train import train, test
 
+from autogluon.tabular import TabularDataset, TabularPredictor
+
+
 def main(args):
     ####################### Setting for Log
     setting = Setting()
@@ -28,7 +31,7 @@ def main(args):
     ######################## DATA PREPROCESS
     print(f'--------------- {args.model} Data PREPROCESSING---------------')
     if args.model in ('XGB'):
-        gbm_data = gbm_dataloader(args)
+        xgb_data = gbm_dataloader(args)
     elif args.model in ('LIGHTGBM'):
         data = lightgbm_preprocess_data(data)
     elif args.model in ('CATBOOST'):
@@ -36,45 +39,64 @@ def main(args):
     else:
         pass
     
-    
+    ######################## Autogluon
+    print(f'--------------- {args.autogluon} ---------------')
+    if args.autogluon == True:
+        train_data, label = data[data['answerCode'] != -1], "answerCode"
+        
+        print(f'--------------- TRAINING ---------------')
+        predictor = TabularPredictor(label=label, eval_metric="roc_auc", problem_type="binary").fit(train_data, presets=["best_quality"])
+        
+        print(f'--------------- PREDICT ---------------')
+        test_data = data[data.dataset == 2]
+        test_data = test_data[test_data["userID"] != test_data["userID"].shift(-1)]
+        test_data = test_data.drop(["answerCode"], axis=1)
+        predicts = predictor.predict_proba(test_data)
+        
+        try:
+            output = predictor.evaluate(test_data, silent=True)
+            valid_auc = output["roc_auc"]
+        except:
+            valid_auc = 0.000
+            
+        os.makedirs(args.saved_model_path, exist_ok=True)
+        with open(f'{args.saved_model_path}/{setting.save_time}_Autogluon_{valid_auc:.3f}_model.pkl', 'wb') as f:
+            pickle.dump(model, f)
+        
+        
+
     ######################## Train/Valid Split
-    print(f'--------------- {args.model} Train/Valid Split ---------------')
-    if args.model in ('XGB'):
-        x_train, y_train, x_valid, y_valid = xgb_datasplit(data)
-    elif args.model in ('LIGHTGBM'):
-        x_train, y_train, x_valid, y_valid = lightgbm_datasplit(args, data)
-    elif args.model in ('CATBOOST'):
-        x_train, y_train, x_valid, y_valid = catboost_datasplit(data)
     else:
-        pass
-    print(f'x_train: {x_train.shape}, y_train: {y_train.shape}, x_valid: {x_valid.shape}, y_valid: {y_valid.shape}')
+        print(f'--------------- {args.model} Train/Valid Split ---------------')
+        if args.model in ('XGB'):
+            x_train, y_train, x_valid, y_valid = xgb_datasplit(data)
+        elif args.model in ('LIGHTGBM'):
+            x_train, y_train, x_valid, y_valid = lightgbm_datasplit(args, data)
+        elif args.model in ('CATBOOST'):
+            x_train, y_train, x_valid, y_valid = catboost_datasplit(data)
+        else:
+            pass
+        print(f'x_train: {x_train.shape}, y_train: {y_train.shape}, x_valid: {x_valid.shape}, y_valid: {y_valid.shape}')
     
     
-    ######################## MODEL LOAD
-    print(f'--------------- {args.model} MODEL LOAD---------------')
-    model = models_load(args)
-    
-    
-    ######################## TRAIN
-    print(f'--------------- {args.model} TRAINING ---------------')
-    model, valid_auc = train(args, model, x_train, y_train, x_valid, y_valid, setting)
+        ######################## MODEL LOAD
+        print(f'--------------- {args.model} MODEL LOAD---------------')
+        model = models_load(args)
+        
+        
+        ######################## TRAIN
+        print(f'--------------- {args.model} TRAINING ---------------')
+        model, valid_auc = train(args, model, x_train, y_train, x_valid, y_valid, setting)
 
 
-    ######################## INFERENCE
-    print(f'--------------- {args.model} PREDICT ---------------')
-    predicts = test(args, model, data)
+        ######################## INFERENCE
+        print(f'--------------- {args.model} PREDICT ---------------')
+        predicts = test(args, model, data)
 
 
     ######################## SAVE PREDICT
-    print(f'--------------- SAVE {args.model} PREDICT ---------------')
+    print(f'--------------- SAVE PREDICT ---------------')
     filename = setting.get_submit_filename(args, valid_auc)
-    
-    write_path = os.path.join(filename)
-    with open(write_path, "w", encoding="utf8") as w:
-        w.write("id,prediction\n")
-        for id, p in enumerate(predicts):
-            w.write("{},{}\n".format(id, p))
-        
     submission = pd.read_csv(args.data_dir + 'sample_submission.csv')
     submission['prediction'] = predicts
 
